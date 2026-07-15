28611f106c0b4f1150798c0cc6acc6039079352b
mryurichiru <mryurichiru@gmail.com>
2026-07-16 00:34:20 +0900

    CommonModelAttribute追加

---
+++ kadai_002/src/main/java/com/example/controller/CommonModelAttribute.java
@@ -0,0 +1,35 @@
+package com.example.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.entity.User;
+import com.example.repository.UserRepository;
+import com.example.security.UserDetailsImpl;
+
+@ControllerAdvice
+public class CommonModelAttribute {
+
+    private final UserRepository userRepository;
+
+    public CommonModelAttribute(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @ModelAttribute("membershipType")
+    public Integer membershipType(Authentication authentication) {
+
+        // 未ログイン
+        if (authentication == null
+                || !(authentication.getPrincipal() instanceof UserDetailsImpl userDetails)) {
+            return null;
+        }
+
+        // DBから最新のユーザー情報を取得
+        User user = userRepository.findById(userDetails.getUser().getId())
+                .orElseThrow(() -> new RuntimeException("ユーザーが見つかりません"));
+
+        return user.getMembershipType();
+    }
+}
